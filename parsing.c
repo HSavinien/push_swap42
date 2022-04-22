1308ab7dc66dcc84da24472a2649eb811f8c55ab
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-04-22 19:08:07 +0200

    parsing finished

--- parsing.c
+++ parsing.c
@@ -6,19 +6,12 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 18:37:17 by tmongell          #+#    #+#             */
-/*   Updated: 2022/04/21 21:17:23 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/04/22 19:05:25 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	check_duplicate(int ac, char **av);
-{
-	while (ac)
-	{
-		if ()
-	}
-}
 void	check_value(char *content)
 {
 	if(content[0] != '-' && content[0] != '+' && !ft_isdigit(content[0]))
@@ -27,29 +20,38 @@ void	check_value(char *content)
 
 int		value_in_tab(int val, int *tab)
 {
-	int i;
+	int			i;
+	static int	nb_zero;
 
 	i = 0;
+	if (!val)
+		nb_zero ++;
+	if (nb_zero > 1)
+		return (1);
 	while(tab[i])
 	{
 		if (tab[i] == val)
 			return (1);
 		i ++;
 	}
+	tab[i] = val;
 	return (0);
 }
 
 t_stack	*read_args(int ac, char **av)
 {
 	t_stack	*element;
+	int		*seen;
 
 	element = NULL;
-	check_duplicate(ac, av);
-	ft_bzero(values, ac * sizeof (int));
+	seen = malloc(sizeof(int) * ac);
+	ft_bzero(seen, ac * sizeof (int));
 	ac --;
 	while (ac)
 	{
 		check_value(av[ac]);
+		if(value_in_tab(ft_atoi(av[ac]), seen))
+			error("duplicated values in stack");
 		element = stack_add_front(element, av[ac]);
 		ac --;
 	}
