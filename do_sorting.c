e46b65074174d1a61472a135a4b7cbb6174071b1
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-04-29 14:27:40 +0200

    minor advencement on sorting system

--- do_sorting.c
+++ do_sorting.c
@@ -6,13 +6,16 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:31:20 by tmongell          #+#    #+#             */
-/*   Updated: 2022/04/25 19:41:22 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/04/29 14:27:06 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+//analize the input and call the best algo depending on the number list. for now, it mostly discriminate between short/long array
 void	do_sorting(stack *sa, stack *sb)
 {
-
+	if (is_stack_sorted(sa))
+		exit(0);
+	if (get_stack_len(sa) )
 }
