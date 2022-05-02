9b96d65360e16ae7303532de4ea78975fa70f9fe
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-05-02 20:33:40 +0200

    end of day

---
+++ moves/actions.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 19:02:52 by tmongell          #+#    #+#             */
+/*   Updated: 2022/05/02 19:05:48 by tmongell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.c"
+
+void	do_swap(t_stack *src)
+{
+	
+}
+
+void	do_push(t_stack *src, t_stack *dst)
+{
+
+}
+
+void	do_rotate(t_stack *src)
+{
+
+}
+
+void	do_rev_rotate(t_stack *src)
+{
+
+}
--- moves/swap.c
+++ moves/swap.c
@@ -1,13 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mv_sa.c                                            :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/30 17:57:20 by tmongell          #+#    #+#             */
-/*   Updated: 2022/04/30 19:16:13 by tmongell         ###   ########.fr       */
+/*   Created: 2022/05/02 19:06:34 by tmongell          #+#    #+#             */
+/*   Updated: 2022/05/02 19:16:29 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
+#include <../push_swap.h>
+
+void	do_swap(t_stack *src)
+{
+
+}
+
+void	mv_sa(t_stack *sa, t_stack *sb)
+{
+	do_swap(sa);
+	printf("sa\n");
+	(void) *sb;
+}
+
+void	mv_sb(t_stack *sa, t_stack *sb)
+{
+
+}
+
+void	mv_ss(t_stack *sa, t_stack *sb)
+{
+
+}
