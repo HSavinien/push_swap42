89bb909f91f5be2144d68c34d973d30049798f38
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-05-04 22:15:46 +0200

    added all rev_rotate movement (compile, not tested)

--- moves/rev_rotate.c
+++ moves/rev_rotate.c
@@ -6,31 +6,43 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 22:43:55 by tmongell          #+#    #+#             */
-/*   Updated: 2022/05/03 22:50:54 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/05/04 22:14:54 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-
-void	do_rev_rotate(t_stack *src)
+static t_stack	*stack_get_penultimate(t_stack *s)
 {
-	
+	while (s->next->next)
+		s = s->next;
+	return (s);
 }
 
-void	mv_rra(t_stack *sa, t_stack *sb)
+void	do_rev_rotate(t_stack **src)
+{
+	t_stack	*tmp;
+
+	tmp = stack_get_penultimate(*src);
+	tmp->next->next = *src;
+	*src = tmp->next;
+	tmp->next = NULL;
+
+}
+
+void	mv_rra(t_stack **sa, t_stack **sb)
 {
 	do_rev_rotate(sa);
 	ft_printf("rra\n");
 	(void) sb;
 }
-void	mv_rrb(t_stack *sa, t_stack *sb)
+void	mv_rrb(t_stack **sa, t_stack **sb)
 {
 	do_rev_rotate(sb);
 	ft_printf("rrb\n");
 	(void) sa;
 }
-void	mv_rrr(t_stack *sa, t_stack *sb)
+void	mv_rrr(t_stack **sa, t_stack **sb)
 {
 	do_rev_rotate(sa);
 	do_rev_rotate(sb);
