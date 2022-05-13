0cfb298b8949941a1743331de589cf60cfe811a9
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-05-13 19:55:50 +0200

    tried to add a special case algo, renonced as it was to much work for to little gain

--- sorting/do_sorting.c
+++ sorting/do_sorting.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:31:20 by tmongell          #+#    #+#             */
-/*   Updated: 2022/05/10 20:01:55 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/05/13 19:11:09 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ void	do_sorting(t_stack *sa, t_stack *sb)
 {
 	int len;
 	len = get_stack_len(sa);
+//	try_rotate_sort(*sa);
 	if (is_stack_sorted(sa))
 		exit(0);
+	if (len == 2)
+		ft_printf("sa\n");
 	else if (len == 3)
 		sort_three(sa, sb);
 	else if (len <= 5)
 		sort_five(sa, sb);
 	else
-		printf("long sort is not yet implemented");//debug
+		ft_printf("long sort is not yet implemented");//debug
 }
